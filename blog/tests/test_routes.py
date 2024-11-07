382a6dba985a16ee81dd577f0b13a5a7e672f4b2
Ilya Savinkin <iasavinkin@gmail.com>
2024-11-07 16:54:17 +0300

    #blogeteria edit and delete tests

--- blog/tests/test_routes.py
+++ blog/tests/test_routes.py
@@ -1,23 +1,31 @@
 from http import HTTPStatus
 
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from blog.models import Category, Post
-from users.models import CustomUser
+from blog.models import Comment, Category, Post
+
+User = get_user_model()
 
 
 class TestRoutes(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls.author = CustomUser.objects.create(username='author')
+        cls.author = User.objects.create(username='author')
+        cls.reader = User.objects.create(username='reader')
         cls.category = Category.objects.create(
             title='title', description='desc', slug='slug'
         )
         cls.post = Post.objects.create(
             title='title', text='text',
             author=cls.author, category=cls.category)
+        cls.comment = Comment.objects.create(
+            text='text',
+            author=cls.author,
+            post=cls.post,
+        )
         return super().setUpTestData()
 
     def test_pages_availibility(self):
@@ -37,3 +45,28 @@ class TestRoutes(TestCase):
                 url = reverse(name, kwargs=kwargs)
                 response = self.client.get(url)
                 self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_availibility_for_edit_and_delete(self):
+        urls = (
+            ('blog:post_edit', {'pk': self.post.pk}),
+            ('blog:post_delete', {'pk': self.post.pk}),
+            ('blog:comment_edit', {'pk': self.comment.pk,
+                                   'post_pk': self.comment.post.pk}),
+            ('blog:comment_delete', {'pk': self.comment.pk,
+                                     'post_pk': self.comment.post.pk}),
+        )
+        self.client.force_login(self.author)
+        for name, kwargs in urls:
+            with self.subTest(name=name):
+                url = reverse(name, kwargs=kwargs)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
+
+        self.client.force_login(self.reader)
+        for name, kwargs in urls:
+            with self.subTest(name=name):
+                url = reverse(name, kwargs=kwargs)
+                redirect_url = reverse(
+                    'blog:post_detail', kwargs={'pk': self.post.pk})
+                response = self.client.get(url)
+                self.assertRedirects(response, redirect_url)
